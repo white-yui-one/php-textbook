d1148bdde188cdf86bfb3dd046fe60c867fdc550
ロックマン <110light03@gmail.com>
2015-05-13 04:35:37 +0000

    0513 practice

---
+++ sample03.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>aaaaa</title>
+</head>
+<body>
+<?php
+printf('おはよう');
+?>
+</body>
+</html>
---
+++ sample04.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    printf('こんばんは');
+    ?>
+    
+</body>
+</html>
